3d785b84ca541e1061d30f839a2af7ea645f6b0b
Md Mohibbullah <mohammedhanif74612@gmail.com>
2025-12-12 20:57:53 +0200

    feat: multer configuration done

---
+++ src/middleware/fileUpload.js
@@ -0,0 +1,30 @@
+import path from "path";
+import multer from "multer";
+const alloweFileExtension = [".png", ".jpeg", ".jpg", ".webP", ".svg"];
+const filter = (req, file, cb) => {
+  if (alloweFileExtension.includes(path.extname(file.originalname))) {
+    cb(null, true);
+  } else {
+    cb(
+      new Error("invalid file! file should be [.png, .jpeg], .jpg,.webP, .svg")
+    );
+  }
+};
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/tem");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "_" + file.originalname;
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const avaterUpload = multer({
+  storage: storage,
+  limits: 5 * 1024 * 1024,
+  filter,
+});
+
+export { avaterUpload };
