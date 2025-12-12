88859caf10c0f61f08d1efd8e5b117c4decebfe6
Md Mohibbullah <mohammedhanif74612@gmail.com>
2025-12-13 01:31:16 +0200

    fix: user.router is fixed

--- src/route/user.router.js
+++ src/route/user.router.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { signUp } from "../controller/user.controller.js";
+import { multerAvaterUpload } from "../middleware/fileUpload.js";
 
 const router = Router();
-router.route("/signup").post(signUp);
+router
+  .route("/signup")
+  .post(multerAvaterUpload.single("restaurantAvater"), signUp);
 
 export default router;
