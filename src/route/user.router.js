e51ab45de554bbd520e681aeb3b82f5bdceb68bf
Md Mohibbullah <mohammedhanif74612@gmail.com>
2025-12-13 21:11:38 +0200

    feat: login route done

--- src/route/user.router.js
+++ src/route/user.router.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { signUp } from "../controller/user.controller.js";
+import { logIn, signUp } from "../controller/user.controller.js";
 import { multerAvaterUpload } from "../middleware/fileUpload.js";
 
 const router = Router();
 router
   .route("/signup")
   .post(multerAvaterUpload.single("restaurantAvater"), signUp);
+router.route("/login").post(logIn);
 
 export default router;
