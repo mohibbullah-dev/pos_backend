665d59796b5bf4798f79a939531f44dd5e021d5d
Md Mohibbullah <mohammedhanif74612@gmail.com>
2025-12-16 11:48:12 +0200

    feat: orderSchema done

---
+++ src/model/order.model.js
@@ -0,0 +1,29 @@
+import mongoose, { mongo, Schema } from "mongoose";
+
+const orderSchema = new Schema(
+  {
+    customerDetails: {
+      name: { type: String, required: true },
+      phone: { type: String, required: true },
+      guests: { type: Number, required: true },
+    },
+    orderStatus: {
+      type: String,
+      required: true,
+    },
+    orderDate: {
+      type: Date,
+      required: true,
+    },
+    bills: {
+      total: { type: Number, required: true },
+      tax: { type: Number, required: true },
+      totalWithTax: { type: Number, required: true },
+    },
+    items: [],
+  },
+
+  { timestamps: true }
+);
+
+export const Order = mongoose.model("Order", orderSchema);
