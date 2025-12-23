49fb53dcb3568612b28ac3b2b4b3444f97cf313f
Md Mohibbullah <mohammedhanif74612@gmail.com>
2025-12-24 01:04:46 +0200

    modified: menu.controller modified

--- src/controller/menu.controller.js
+++ src/controller/menu.controller.js
@@ -1,3 +1,4 @@
+import { Menu } from "../model/menu.model";
 import { apiError } from "../utils/apiError";
 import asyncHandler from "../utils/asyncHandler";
 
@@ -7,4 +8,19 @@ const menuCreate = asyncHandler(async (req, res) => {
     throw new apiError(400, "all fields are required");
   if (Object.keys(dishes).length <= 0)
     throw new apiError(400, "dihes are required");
+
+  const resutl = await Menu.findOneAndUpdate(
+    { name },
+    { name, icon, color, dishes },
+    { new: true, upsert: true, includeResultMetadata: true }
+  );
+  if (!resutl.value) throw new apiError(500, "menu creation faild!");
+
+  const created = resutl.lastErrorObject.updatedExisting === false;
+
+  return res
+    .status(created ? 201 : 200)
+    .json(created ? 201 : 200, resutl.value);
 });
+
+export { menuCreate };
